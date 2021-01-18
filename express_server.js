0234c404ab092824cbed90c1e944ca9d94c61bdd
janiapurva <jani.apurva@yahoo.com>
2021-01-18 17:45:16 -0500

    adding new route

--- express_server.js
+++ express_server.js
@@ -33,10 +33,15 @@ app.get('/urls' , (req,res) => {
   res.render('urls_index',templateVars);
 });
 
+// adding  Get route to Show the Form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //adding second route and templete
 app.get('/urls/:shortURL' ,(req,res) => {
-const templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
-res.render('urls_show',templateVars);
+  const templateVars = {shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
+  res.render('urls_show',templateVars);
 });
 
 
