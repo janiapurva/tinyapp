6479e0bd355c442fc7d695391ecd9380f6c2dd28
janiapurva <jani.apurva@yahoo.com>
2021-01-18 13:43:36 -0500

    adding server express file and basci code

---
+++ express_server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+const urlDatabase = {
+  "b2xVn2" : "http://www.lighthouselabs.ca",
+  "9sm5xk" :  "http://www.google.com"
+};
+
+// creating homePage request
+app.get('/', (req,res) => {
+  res.send('Hello!');
+});
+
+//listening port 
+app.listen(PORT, () => {
+  console.log(`Example app lsitening on port ${PORT}!`);
+});
+
